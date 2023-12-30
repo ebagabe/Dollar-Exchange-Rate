b2717763a835dae729d1e695ec66074274995191
ebagabe <ebagabee@gmail.com>
2023-12-30 13:32:22 -0300

    Ended aplication

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const request = require('request')
     Requisições feitas através do site: https://docs.awesomeapi.com.br/ 
 */
 
-const coins = 'USD-BRL, EUR-BRL, BTC-BRL'
+const coins = 'USD-BRL,EUR-BRL,BTC-BRL'
 
 const options = {
     url: `https://economia.awesomeapi.com.br/last/${coins}`,
@@ -20,4 +20,34 @@ const callbackAllExchangeRate = function (error, res, body) {
     console.log(json);
 }
 
-request(options, callbackAllExchangeRate)
+const dolarCallback = function (error, res, body) {
+    let json = JSON.parse(body)
+    const price = json.USDBRL['bid']
+    const day = json.USDBRL['create_date']
+    console.log("VALOR DO DOLAR ATUAL")
+    console.log(`Dolar: R$ ${price}, Dia: ${day}`);
+}
+
+const euroCallback = function (error, res, body) {
+    let json = JSON.parse(body)
+    const price = json.EURBRL['bid']
+    const day = json.EURBRL['create_date']
+    console.log("VALOR DO EURO ATUAL")
+    console.log(`Euro: R$ ${price}, Dia: ${day}`);
+}
+
+const btcCallback = function (error, res, body) {
+    let json = JSON.parse(body)
+    const price = json.BTCBRL['bid']
+    const day = json.BTCBRL['create_date']
+    console.log("VALOR DO BITCOIN ATUAL")
+    console.log(`Bitcoin: R$ ${price}, Dia: ${day}`);
+}
+
+setInterval(() => {
+    request(options, dolarCallback)
+    request(options, euroCallback)
+    request(options, btcCallback)
+    console.log("NOVA REQUISIÇÃO\n")
+}, 5000)
+
